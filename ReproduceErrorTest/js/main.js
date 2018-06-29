b27b46cba658c6728d64d0e8eaff18e92eed3af1
Jim Cloudman <jim@jimcloudman.com>
2018-06-28 23:15:43 -0500

    Working in this state

--- ReproduceErrorTest/js/main.js
+++ ReproduceErrorTest/js/main.js
@@ -1,1 +1,7 @@
 ﻿// Your code here!
+
+var test = new RuntimeComponent1.Class1;
+
+test.getExample().then(result => {
+    console.log(result);
+});
---
+++ RuntimeComponent1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace RuntimeComponent1
+{
+    public sealed class Class1
+    {
+        public IAsyncOperation<string> GetExample()
+        {
+            return AsyncInfo.Run(token => Task.Run(getExample));
+        }
+
+        private async Task<string> getExample()
+        {
+            return "It's working";
+        }
+    }
+}
